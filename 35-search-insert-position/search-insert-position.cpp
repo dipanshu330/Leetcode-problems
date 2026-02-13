ed38a377d4caec00031c7e0e9280e34f5ec03397
dipanshu330 <dipanshuchaudhary3006@gmail.com>
2026-02-13 23:46:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (98.01%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n= nums.size();
+        int l=0,h=n-1;
+        
+        while(l<=h)       
+        {
+            int mid=(l+h)/2;
+            if(nums[mid]== target)
+            return mid;
+            else if(nums[mid]< target)
+            l=mid+1;
+            else if(nums[mid]> target)
+            h=mid-1;
+
+        }
+        return l;
+        
+    }
+};
